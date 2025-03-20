02f00e5d1526977801d736bbf7ebc5ca78bf8636
Lugame Cooper <legamerfdp81@gmail.com>
2025-03-20 17:28:08 +0100

    .

--- login.php
+++ login.php
@@ -12,6 +12,7 @@
             include './connect.php';
             $res = $connexion -> query("SELECT * FROM visiteur WHERE nom = '$_POST[login]' AND password = '$_POST[password]'") or die("test");
             $res = $res -> fetch();
+            echo "<p>test</p>";
             if ($res["idRole"] == 2){
                 $_COOKIE["info_log"] = $res;
                 //echo '<script>window.location.replace("https://gsb.lucas-lestiennes.fr/comptable")</script>';
