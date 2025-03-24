6d0988aae8881125393d755a05751fb12effff44
FIRST_NAME LAST_NAMESarahKergoCaz <sarah.kergosien.cazorla@gmail.com>
2025-03-24 20:11:04 +0100

    .

--- logoff.php
+++ logoff.php
@@ -1,4 +1,6 @@
 <?php 
+    $_SESSION["idRole"] = NULL;
+    $_SESSION["idUser"] = NULL;
     session_destroy();
     echo '<script>window.location.replace("https://gsb.lucas-lestiennes.fr/?erreur=vous avez été déconecter")</script>';
 ?>
