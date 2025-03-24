649a8441bc72b32acb2dfd8428eda441f42b171d
manu321-code <emmanuel.tchertchian.32@gmail.com>
2025-03-24 21:03:13 +0100

    adaptation form into login page

--- index.php
+++ index.php
@@ -8,24 +8,28 @@
         <title>Intranet GSB</title>
     </head>
     <body>
-        <div class="container">
-            <form action="login.php" method="post">
-                <img class="logo" alt="logo GSB" src="./src/logo-gsb.png">
-                <p>Bienvenue chez GSB</p>
-                <input type="login"    name="login"    placeholder="Nom d'utilisateur"><br>
-                <input type="password" name="password" placeholder="Mot de passe"><br>
-                <input type="submit" value="Connexion"><br>
-                <?php 
+        <div class="wrapper">
+            <div class="container">
+                <form action="login.php" method="post">
+                    <img class="logo" alt="logo GSB" src="./src/logo-gsb.png">
+                    <p>Bienvenue chez GSB</p>
+                    <input type="login" name="login"
+                        placeholder="Nom d'utilisateur"><br>
+                    <input type="password" name="password"
+                        placeholder="Mot de passe"><br>
+                    <input type="submit" value="Connexion"><br>
+                    <?php
                     error_reporting(E_ERROR | E_PARSE);
                     try{
-                        if ($_GET["erreur"]){
-                            echo "<p>$_GET[erreur]</p>";
-                        }
-                    }catch (Echec) {
-                        NULL;
+                    if ($_GET["erreur"]){
+                    echo "<p>$_GET[erreur]</p>";
                     }
-                ?>
-            </form>
+                    }catch (Echec) {
+                    NULL;
+                    }
+                    ?>
+                </form>
+            </div>
         </div>
     </body>
 </html>
