647c0820dc31c9b59c333de849e4e1a2b41c940d
FIRST_NAME LAST_NAMESarahKergoCaz <sarah.kergosien.cazorla@gmail.com>
2025-03-24 22:55:06 +0100

    reprise complete du css (déso manu)

--- index.php
+++ index.php
@@ -5,31 +5,27 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" type="image/x-icon" href="./src/logo.ico">
         <link rel="stylesheet" href="style.css">
-        <title>Intranet GSB</title>
+        <title> Intranet GSB </title>
     </head>
     <body>
-        <div class="wrapper">
-            <div class="container">
+            <div class="login_form">
                 <form action="login.php" method="post">
                     <img class="logo" alt="logo GSB" src="./src/logo-gsb.png">
                     <p>Bienvenue chez GSB</p>
-                    <input type="login" name="login"
-                        placeholder="Nom d'utilisateur"><br>
-                    <input type="password" name="password"
-                        placeholder="Mot de passe"><br>
+                    <input type="login" name="login" placeholder="Nom d'utilisateur" autocapitalize="false" ><br>
+                    <input type="password" name="password" placeholder="Mot de passe"><br>
                     <input type="submit" value="Connexion"><br>
                     <?php
-                    error_reporting(E_ERROR | E_PARSE);
-                    try{
-                    if ($_GET["erreur"]){
-                    echo "<p>$_GET[erreur]</p>";
-                    }
-                    }catch (Echec) {
-                    NULL;
-                    }
+                        error_reporting(E_ERROR | E_PARSE);
+                        try{
+                            if ($_GET["erreur"]){
+                                echo "<p>$_GET[erreur]</p>";
+                            }
+                        }catch(Error){
+                            NULL;
+                        }
                     ?>
                 </form>
             </div>
-        </div>
     </body>
 </html>
