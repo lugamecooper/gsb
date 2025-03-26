d48dd7a4c07208ca93ef7c5f19336f7a016b4656
manu321-code <emmanuel.tchertchian.32@gmail.com>
2025-03-26 09:15:44 +0100

    try to correct ccs problem #1

--- comptable/index.php
+++ comptable/index.php
@@ -20,7 +20,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Intranet - Comptable</title>
-        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="./style.css">
     </head>
     <body>
 
