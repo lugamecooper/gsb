506895044dd5b18bbffbc55963e3e72707281059
manu321-code <emmanuel.tchertchian.32@gmail.com>
2025-03-26 09:45:15 +0100

    add correct nav bar to comptable

--- comptable/index.php
+++ comptable/index.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
-<?php 
-    include './connect.php';
-    session_start();
-    if (isset($_SESSION["IdVisiteur"])) {
-        header("Location: https://gsb.lucas-lestiennes.fr/?erreur=veuillez vous connecter");
-    }
-    else{
-        if ($_SESSION["idRole"] == 2 || $_SESSION["idRole"] == 3) {
-            NULL;
-        } elseif ($_SESSION["idRole"] == 1) {
-            header("Location: https://gsb.lucas-lestiennes.fr/visiteur");
-        } else {
-            header("Location: https://gsb.lucas-lestiennes.fr/?erreur=veuillez vous connecter");
-        }
-    }
+<?php
+include './connect.php';
+session_start();
+if (isset($_SESSION["IdVisiteur"])) {
+header("Location: https://gsb.lucas-lestiennes.fr/?erreur=veuillez vous connecter");
+}
+else{
+if ($_SESSION["idRole"] == 2 || $_SESSION["idRole"] == 3) {
+NULL;
+} elseif ($_SESSION["idRole"] == 1) {
+header("Location: https://gsb.lucas-lestiennes.fr/visiteur");
+} else {
+header("Location: https://gsb.lucas-lestiennes.fr/?erreur=veuillez vous connecter");
+}
+}
 ?>
 <html lang="fr">
     <head>
@@ -28,29 +28,30 @@
             <h1>Intranet Comptable</h1>
         </div>
 
-        <ul>
+        <ul class="nav-bar">
             <li><a href=".\">Acceuil</a></li>
-            <li><a href=".\validation_frais.html">Validation des fiches de frais</a></li>
-            <li><a href=".\suivi_paiements.html">Suivi des paiements</a></li>
-            <li style="float:right"><a class="active"
-                    href="#logo">LOGO</a></li>
-            <li style="float:right"><a class="active" href="#utilisateur">Nom d'utilisateur</a></li>
-            <li style="float:right"><a class="active" href="#fonction">Comptable</a></li>
-            <li style="float:right"><a class="active"
-                    href="../logoff.php">Déconnection</a></li>
+            <li><a href=".\saisie_frais">Saisi de frais</a></li>
+            <li><a href=".\saisie_frais">Consultation fiche de frais</a></li>
+            <div class="user-info">
+                <li style="float:right"><a class="active"
+                        href="#logo">LOGO</a></li>
+                <li style="float:right"><a class="active"
+                        href="#utilisateur">Nom d'utilisateur</a></li>
+                <li style="float:right"><a class="active"
+                        href="#fonction">Visiteur médical</a></li>
+                <li style="float:right"><a class="active"
+                        href="../logoff.php">Déconnection</a></li>
+            </div>
         </ul>
 
         <div class="container">
             <h3>Validation des fiches de frais</h3>
-
             <p>Sélection d’un visiteur et d’un mois <br>
                 Affichage des détails de la fiche <br>
                 Modification des frais forfaitisés <br>
                 Suppression ou report des frais hors forfait <br>
                 Bouton pour valider la fiche</p>
-
             <h3>Suivi des paiements</h3>
-
             <p>Liste des fiches en attente de paiement <br>
                 Détail des fiches mises en paiement <br>
                 Bouton pour marquer comme "Remboursé"</p>
--- visiteur/index.php
+++ visiteur/index.php
@@ -1,51 +1,59 @@
 <!DOCTYPE html>
-<?php 
-    include './connect.php';
-    session_start();
-    if (isset($_SESSION["IdVisiteur"])) {
-        header("Location: https://gsb.lucas-lestiennes.fr/?erreur=veuillez vous connecter");
-    }
-    else{
-        if ($_SESSION["idRole"] == 1 || $_SESSION["idRole"] == 3) {
-            NULL;
-        } elseif ($_SESSION["idRole"] == 2) {
-            header("Location: https://gsb.lucas-lestiennes.fr/comptable");
-        } else {
-            header("Location: https://gsb.lucas-lestiennes.fr/?erreur=veuillez vous connecter");
-        }
-    }
+<?php
+include './connect.php';
+session_start();
+if (isset($_SESSION["IdVisiteur"])) {
+header("Location: https://gsb.lucas-lestiennes.fr/?erreur=veuillez vous connecter");
+}
+else{
+if ($_SESSION["idRole"] == 1 || $_SESSION["idRole"] == 3) {
+NULL;
+} elseif ($_SESSION["idRole"] == 2) {
+header("Location: https://gsb.lucas-lestiennes.fr/comptable");
+} else {
+header("Location: https://gsb.lucas-lestiennes.fr/?erreur=veuillez vous connecter");
+}
+}
 ?>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Intranet - Visiteurs médicaux</title>
-    <link rel="stylesheet" href="./style.css">
-</head>
-<body>
-    <div class="header">
-        <h1>Intranet visiteurs médicaux</h1>
-    </div>
-    <ul class="nav-bar">
-        <li><a href=".\">Acceuil</a></li>
-        <li><a href=".\saisie_frais">Saisi de frais</a></li>
-        <li><a href=".\saisie_frais">Consultation fiche de frais</a></li>
-        <div class="user-info">
-            <li style="float:right"><a class="active" href="#logo">LOGO</a></li>
-            <li style="float:right"><a class="active" href="#utilisateur">Nom d'utilisateur</a></li>
-            <li style="float:right"><a class="active" href="#fonction">Visiteur médical</a></li>
-            <li style="float:right"><a class="active" href="../logoff.php">Déconnection</a></li>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Intranet - Visiteurs médicaux</title>
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+        <div class="header">
+            <h1>Intranet visiteurs médicaux</h1>
         </div>
-    </ul>
-    <div class="container">
-        <h3>Saisie des frais</h3>
-        <p>Formulaire pour entrer les frais forfaitisés (quantité pour chaque type) <br>
-           Formulaire pour ajouter des frais hors forfait (date, libellé,montant) <br>
-           Liste des frais saisis avec possibilité de modification/suppression</p>
-        <h3>Consultation des fiches de frais</h3>
-        <p>Sélecteur de mois pour afficher une fiche de frais <br>
-           Tableau avec les frais forfaitisés et hors forfait <br>
-           Indicateur d'état de la fiche (saisie, validée, mise en paiement, remboursée)</p>
-    </div>
-</body>
+        <ul class="nav-bar">
+            <li><a href=".\">Acceuil</a></li>
+            <li><a href=".\saisie_frais">Saisi de frais</a></li>
+            <li><a href=".\saisie_frais">Consultation fiche de frais</a></li>
+            <div class="user-info">
+                <li style="float:right"><a class="active"
+                        href="#logo">LOGO</a></li>
+                <li style="float:right"><a class="active"
+                        href="#utilisateur">Nom d'utilisateur</a></li>
+                <li style="float:right"><a class="active"
+                        href="#fonction">Visiteur médical</a></li>
+                <li style="float:right"><a class="active"
+                        href="../logoff.php">Déconnection</a></li>
+            </div>
+        </ul>
+        <div class="container">
+            <h3>Saisie des frais</h3>
+            <p>Formulaire pour entrer les frais forfaitisés (quantité pour
+                chaque type) <br>
+                Formulaire pour ajouter des frais hors forfait (date,
+                libellé,montant) <br>
+                Liste des frais saisis avec possibilité de
+                modification/suppression</p>
+            <h3>Consultation des fiches de frais</h3>
+            <p>Sélecteur de mois pour afficher une fiche de frais <br>
+                Tableau avec les frais forfaitisés et hors forfait <br>
+                Indicateur d'état de la fiche (saisie, validée, mise en
+                paiement, remboursée)</p>
+        </div>
+    </body>
 </html>
