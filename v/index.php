ed258595c9fa9bf48a45574e4a5de8ed7615d235
manu321-code <emmanuel.tchertchian.32@gmail.com>
2025-03-27 23:57:37 +0100

    test new Navbar

--- v/index.php
+++ v/index.php
@@ -26,6 +26,7 @@
         <div class="header">
             <h1>Intranet visiteurs médicaux</h1>
         </div>
+
         <ul class="nav-bar">
             <li><a href="./">Acceuil</a></li>
             <li><a href="./sf">Saisi de frais</a></li>
@@ -45,6 +46,24 @@
                 </li>
             </div>
         </ul>
+
+
+        <nav class="navbar">
+            <ul class="centered-links">
+                <li><a href="./">Acceuil</a></li>
+                <li><a href="./sf">Saisi de frais</a></li>
+                <li><a href="./cf">Consultation fiche de frais</a></li>
+            </ul>
+            <ul class="right-aligned">
+                <li><p>LOGO</p></li>
+                <li><p>Nom d'utilisateur</p></li>
+                <li><a href="#fonction">Visiteur médical</a></li>
+                <li><a href="../logoff.php">Déconnection</a></li>
+            </ul>
+        </nav>
+
+
+
         <div class="container">
             <h3>Saisie des frais</h3>
             <p>
