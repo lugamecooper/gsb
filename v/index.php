79282e9ef83ae15f70e89663954f1d0592e9de3f
manu321-code <emmanuel.tchertchian.32@gmail.com>
2025-03-28 00:26:16 +0100

    implement color logoff !!BUG!!

--- v/index.php
+++ v/index.php
@@ -37,7 +37,7 @@
                 <li><p>LOGO</p></li>
                 <li><p>Nom d'utilisateur</p></li>
                 <li><a href="#fonction">Visiteur médical</a></li>
-                <li><a href="../logoff.php">Déconnection</a></li>
+                <li class="bouton_logoff"><a href="../logoff.php">Déconnection</a></li>
             </ul>
         </nav>
 
