5382f98689c9aee0d48c23e7f1357539a6152712
manu321-code <emmanuel.tchertchian.32@gmail.com>
2025-03-27 17:43:05 +0100

    .

--- visiteur/consultation_frais/index.php
+++ visiteur/consultation_frais/index.php
@@ -14,7 +14,7 @@
 
         <ul>
             <li><a href="../">Acceuil</a></li>
-            <li><a href="./saisie_frais">Saisi de frais</a></li>
+            <li><a href="../saisie_frais">Saisi de frais</a></li>
             <li><a href="./">Consultation fiche de frais</a></li>
             <li style="float:right"><a class="active"
                     href="#logo">LOGO</a></li>
