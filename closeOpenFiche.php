4449ef3150e335beccbcdcbc15a891a546f80b6f
FIRST_NAME LAST_NAMESarahKergoCaz <sarah.kergosien.cazorla@gmail.com>
2025-05-02 15:41:27 +0200

    .

--- closeOpenFiche.php
+++ closeOpenFiche.php
@@ -1,16 +1,17 @@
 <?php
     function close($connexion,$idUser = NULL){
         $month = date('n');
-            if ($idUser){
-                $res = $connexion -> query("SELECT * FROM FicheFrais WHERE IdEtat = 1 AND Mois != $month AND IdVisiteur = $idUser;") -> fetchAll();
+        $year = date('n');
+        if ($idUser){
+            $res = $connexion -> query("SELECT * FROM FicheFrais WHERE IdEtat = 1 AND Mois != $month AND IdVisiteur = $idUser;") -> fetchAll();
+        }
+        else{
+            $res = $connexion -> query("SELECT * FROM FicheFrais WHERE IdEtat = 1 AND Mois != $month;") -> fetchAll();
+        }
+        if ($res != FALSE){
+            foreach ($res as $x) {
+                $connexion -> exec("UPDATE FicheFrais SET IdEtat = 2 WHERE IdVisiteur = $x[IdVisiteur] AND $x[Mois] != $month");
             }
-            else{
-                $res = $connexion -> query("SELECT * FROM FicheFrais WHERE IdEtat = 1 AND Mois != $month;") -> fetchAll();
-            }
-            if ($res != FALSE){
-                foreach ($res as $x) {
-                    $connexion -> exec("UPDATE FicheFrais SET IdEtat = 2 WHERE IdVisiteur = $x[IdVisiteur] AND $x[Mois] != $month");
-                }
-            }
+        }
     }
 ?>
