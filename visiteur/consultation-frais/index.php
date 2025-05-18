c7cc3ace53188463746f637104b591641b07b805
Lugame Cooper <legamerfdp81@gmail.com>
2025-05-18 22:14:50 +0200

    .

--- visiteur/consultation-frais/index.php
+++ visiteur/consultation-frais/index.php
@@ -28,7 +28,7 @@
             <h2>Consulter mes fiches de frais</h2>
             <form>
                 <label>Période</label> <br>
-                <input type="date" placeholder="selectioner la date"><br>
+                <input type="month" placeholder="selectioner la date"><br>
                 <button type="submit">Afficher mes fiches</button>
             </form>
         </div>
