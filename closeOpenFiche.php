8a355b886615f7e4a000c484b3cc7b8193098a82
Lugame Cooper <legamerfdp81@gmail.com>
2025-05-18 21:37:38 +0200

    .

--- closeOpenFiche.php
+++ closeOpenFiche.php
@@ -16,7 +16,7 @@
         }
         else{
             $connexion -> exec("INSERT INTO FicheFrais(IdVisiteur, Mois, nbJustificatifs, dateModif, montantValide, IdEtat) VALUES('$_SESSION[idUser]', '$month', 0, '$day', 0, 1)");
-            exit();
+            return ;
         }
     }
 ?>
