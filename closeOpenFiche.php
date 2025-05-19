cdadc1f0977aa6c56eb77efa473e651df9e8ae1d
Lugame Cooper <legamerfdp81@gmail.com>
2025-05-19 07:07:35 +0200

    .

--- closeOpenFiche.php
+++ closeOpenFiche.php
@@ -4,22 +4,22 @@
         $day = date("Y-m-d");
         if ($idUser){
             try{
-                $res = $connexion -> query("SELECT * FROM FicheFrais WHERE IdEtat = 1 AND Mois != $month AND IdVisiteur = $idUser;") -> fetchAll();
+                $res = $connexion -> query("SELECT * FROM FicheFrais WHERE IdEtat = 'CR' AND Mois != $month AND IdVisiteur = $idUser;") -> fetchAll();
             }catch(PDOException $e){
                 $res = FALSE;
             }
         }
         else{
-            $res = $connexion -> query("SELECT * FROM FicheFrais WHERE IdEtat = 1 AND Mois != $month;") -> fetchAll();
+            $res = $connexion -> query("SELECT * FROM FicheFrais WHERE IdEtat = 'CR' AND Mois != $month;") -> fetchAll();
         }
         if ($res != FALSE){
             foreach ($res as $x) {
-                $connexion -> exec("UPDATE FicheFrais SET IdEtat = 2 WHERE IdVisiteur = $x[IdVisiteur] AND $x[Mois] != $month");
+                $connexion -> exec("UPDATE FicheFrais SET IdEtat = 'CL' WHERE IdVisiteur = $x[IdVisiteur] AND $x[Mois] != $month");
             }
         }
         else{
             if(!$connexion -> query("SELECT * FROM FicheFrais WHERE Mois = $month AND IdVisiteur = $_SESSION[idUser]") -> fetch())
-                $connexion -> exec("INSERT INTO FicheFrais(IdVisiteur, Mois, nbJustificatifs, dateModif, montantValide, IdEtat) VALUES('$_SESSION[idUser]', '$month', 0, '$day', 0, 1)");
+                $connexion -> exec("INSERT INTO FicheFrais(IdVisiteur, Mois, nbJustificatifs, dateModif, montantValide, IdEtat) VALUES($_SESSION[idUser], '$month', 0, '$day', 0, 'CR')");
         }
     }
 ?>
