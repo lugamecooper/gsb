1a4d6bc33060482c7632408d42747cbf2ac95184
Lugame Cooper <legamerfdp81@gmail.com>
2025-05-20 04:26:38 +0200

    sdf

--- visiteur/saisie-frais/index.php
+++ visiteur/saisie-frais/index.php
@@ -63,7 +63,8 @@
                                     </form>";
                         }
                     ?>
-                    <tr><td><input placeholder="entrer le libelle" name="new_libelle"/></td><td>Sélectionner la date:<input type="date" name="new_date"></td><td>Montant:<input type="number" name="new_price"></td>
+                    <tr></tr>
+                    <tr><td><input placeholder="entrer le libelle" name="new_libelle"/></td><td>Sélectionner la date:<input type="date" name="new_date"></td><td>Montant:<input type="number" name="new_price"></td></tr>
                 </table>
                 <input type="submit" value="Soumettre"/>
             </form>
