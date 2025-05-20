4a363263cda4ba97ebeca9ec2b0606e67f0a866b
Lugame Cooper <legamerfdp81@gmail.com>
2025-05-20 03:26:45 +0200

    sdf

--- visiteur/saisie-frais/index.php
+++ visiteur/saisie-frais/index.php
@@ -38,9 +38,9 @@
                 <table>
                     <?php
                         $month = date('n');
-                        $res_2 = $connexion -> query("SELECT libelle, quantite FROM LigneFraisForfait INNER JOIN FraisForfais ON FraisForfais.idFrais = LigneFraisForfait.idFrais WHERE Mois = $month AND IdVisiteur = $_SESSION[idUser]; ") -> fetchAll();
+                        $res_2 = $connexion -> query("SELECT libelle, quantite, LigneFraisForfait.idFrais FROM LigneFraisForfait INNER JOIN FraisForfais ON FraisForfais.idFrais = LigneFraisForfait.idFrais WHERE Mois = $month AND IdVisiteur = $_SESSION[idUser]; ") -> fetchAll();
                         foreach ($res_2 as $e){
-                            echo "<tr><td><label for=\"number_$e[0]\">$e[0]</label></td><td><input style=\"margin-left:5px;\" required=\"required\" type=\"number_$e[0]\" id=\"number_$e[0]\" name=\"number_$e[0]\" value=\"$e[1]\"></td>";
+                            echo "<tr><td><label for=\"number_$e[2]\">$e[0]</label></td><td><input style=\"margin-left:5px;\" required=\"required\" type=\"number_$e[2]\" id=\"number_$e[2]\" name=\"number_$e[2]\" value=\"$e[1]\"></td>";
                         }
                     ?>
                 </table>
