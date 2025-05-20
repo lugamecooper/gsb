1b9654f8737938c272ffdc99c1f1a4ea3793ab53
Lugame Cooper <legamerfdp81@gmail.com>
2025-05-20 05:21:32 +0200

    ef

--- comptable/suivie-paiment/index.php
+++ comptable/suivie-paiment/index.php
@@ -25,12 +25,9 @@
     <body>
         <?php include "../include/header.inc.php"; header_element("..", "..", "..", "../..");?>
         <div class="container">
-        <h2>Suivre le paiement des fiches de frais</h2>
+        <h2>Suivre la mise en paiement des fiches de frais</h2>
             <form>
-                <label>Numéro de fiche de frais</label>
-                <input type="number" required> <br>
                 
-                <button type="submit">Vérifier l'état du paiement</button>
             </form>
         </div>
     </body>
