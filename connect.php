3efdbc7b25d2ddd5bad7688f593c3ec2e9a0cb95
Lugame Cooper <legamerfdp81@gmail.com>
2025-05-21 09:01:11 +0200

    change pwd and usr sql

--- connect.php
+++ connect.php
@@ -1,8 +1,7 @@
 <?php
     $dsn = 'mysql:host=localhost;dbname=GSB;charset=utf8';
-    $user = 'root';
-    //$password = '';
-    $password = "root";
+    $user = 'app';
+    $password = "r^J\$X_Sq&4t4rKu/E.VG";
     try {
         $connexion = new PDO($dsn, $user, $password);
     } catch (PDOException $e) {
